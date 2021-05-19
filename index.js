363c57f0218924f76eacbe4c8604428a149a4d71
Sindhudesu <>
2021-05-19 20:18:50 +0000

    line

--- index.js
+++ index.js
@@ -1,8 +1,62 @@
 // FORK this starter file and save it to your own Repl.it account.
+/*
 
+Variable	Value
+date	Monday 2019-03-18
+time	10:05:34 AM
+astronautCount	7
+astronautStatus	ready
+averageAstronautMassKg	80.7
+crewMassKg	astronautCount * averageAstronautMassKg
+fuelMassKg	760,000
+shuttleMassKg	74842.31
+totalMassKg	crewMassKg + fuelMassKg + shuttleMassKg
+fuelTempCelsius	-225
+fuelLevel	100%
+weatherStatus	clear
+
+-------------------------------------
+> LC04 - LAUNCH CHECKLIST
+-------------------------------------
+Date: Monday 2019-03-18
+Time: 10:05:34 AM
+
+-------------------------------------
+> ASTRONAUT INFO
+-------------------------------------
+* count: 7
+* status: ready
+
+-------------------------------------
+> FUEL DATA
+-------------------------------------
+* Fuel temp celsius: -225 C
+* Fuel level: 100%
+
+-------------------------------------
+> MASS DATA
+-------------------------------------
+* Crew mass: 564.9 kg
+* Fuel mass: 760000 kg
+* Shuttle mass: 74842.31 kg
+* Total mass: 835407.21 kg
+
+-------------------------------------
+> FLIGHT PLAN
+-------------------------------------
+* weather: clear
+
+-------------------------------------
+> OVERALL STATUS
+-------------------------------------
+* Clear for takeoff: YES
+
+
+*/
 
 // Declare and initialize the 12 variables here:
 
+console.log("__________");
 
 
 
