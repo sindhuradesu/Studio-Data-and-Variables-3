395dc04d002e29b1392d83e8b8ab5d1e725234b4
Sindhudesu <>
2021-05-19 21:38:28 +0000

    data and variables

--- index.js
+++ index.js
@@ -1,70 +1,61 @@
 // FORK this starter file and save it to your own Repl.it account.
-/*
+/*const input=require('readline-sync');
 
-Variable	Value
-date	Monday 2019-03-18
-time	10:05:34 AM
-astronautCount	7
-astronautStatus	ready
-averageAstronautMassKg	80.7
-crewMassKg	astronautCount * averageAstronautMassKg
-fuelMassKg	760,000
-shuttleMassKg	74842.31
-totalMassKg	crewMassKg + fuelMassKg + shuttleMassKg
-fuelTempCelsius	-225
-fuelLevel	100%
-weatherStatus	clear
-
--------------------------------------
-> LC04 - LAUNCH CHECKLIST
--------------------------------------
-Date: Monday 2019-03-18
-Time: 10:05:34 AM
-
--------------------------------------
-> ASTRONAUT INFO
--------------------------------------
-* count: 7
-* status: ready
-
--------------------------------------
-> FUEL DATA
--------------------------------------
-* Fuel temp celsius: -225 C
-* Fuel level: 100%
-
--------------------------------------
-> MASS DATA
--------------------------------------
-* Crew mass: 564.9 kg
-* Fuel mass: 760000 kg
-* Shuttle mass: 74842.31 kg
-* Total mass: 835407.21 kg
-
--------------------------------------
-> FLIGHT PLAN
--------------------------------------
-* weather: clear
-
--------------------------------------
-> OVERALL STATUS
--------------------------------------
-* Clear for takeoff: YES
-
-
+let astronautCount= input.question("what is astronautCount");
+astronautCount=Number(astronautCount);
 */
-
 // Declare and initialize the 12 variables here:
 
-console.log("hello world");
-
-
+let date=" Monday 2019-03-18";
+let time=" 10:05:34 AM";
+let astronautCount=7;
+let astronautStatus=" ready";
+let averageAstronautMassKg=80.7;
+let crewMassKg=	astronautCount * averageAstronautMassKg;
+let fuelMassKg	=760000;
+let shuttleMassKg	=74842.31;
+let totalMassKg	=crewMassKg + fuelMassKg + shuttleMassKg;
+let fuelTempCelsius	=-225;
+let fuelLevel	=1;
+let weatherStatus	=" clear";
 
 // Write code to generate the LC04 report here:
-
-
-
-
+let line="-------------------------------------";
+console.log(line);
+console.log("> LC04 - LAUNCH CHECKLIST");
+console.log(line);
+console.log("Date:"+date);
+console.log("Time:"+time);
+console.log();
+console.log(line);
+console.log("> ASTRONAUT INFO");
+console.log(line);
+console.log("* count: "+astronautCount);
+console.log("* status: "+astronautStatus);
+console.log();
+console.log(line);
+console.log("> FUEL DATA");
+console.log(line);
+console.log("*Fuel temp celsius: "+fuelTempCelsius	+"c");
+console.log("*Fuel level:"+ fuelLevel*100+"%");
+console.log();
+console.log(line);
+console.log(">MASS DATA");
+console.log(line);
+console.log("* Crew mass: "+crewMassKg+"kg");
+console.log("* Fuel mass: "+fuelMassKg+"kg");
+console.log("* Shuttle mass: "+shuttleMassKg+"kg");
+console.log("* Total mass: "+totalMassKg+"kg");
+console.log();
+console.log(line);
+console.log("> FLIGHT PLAN");
+console.log(line);
+console.log("* weather: "+weatherStatus);
+console.log();
+console.log(line);
+console.log(">OVERALL STATUS");
+console.log(line);
+console.log("* Clear for takeoff: YES");
 // When done, have your TA check your code.
 
 
